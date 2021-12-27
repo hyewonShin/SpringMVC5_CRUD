07ea273c2d72d470ae1b322d056e4893165cb80d
hyewonShin <rusy7225@naver.com>
2021-12-27 16:49:19 +0900

    글 삭제하기 글을 삭제하는 기능을 구현한다. 삭제 버튼을 누르면 글을 삭제하고 글 목록 페이지로 이동한다.

--- MiniProjectJava/src/kr/co/hyewon/service/BoardService.java
+++ MiniProjectJava/src/kr/co/hyewon/service/BoardService.java
@@ -78,4 +78,12 @@ public class BoardService {
 		
 		boardDao.modifyContentInfo(modifyContentBean);
 	}
+	
+	public void deleteContentInfo(int content_idx) {
+		boardDao.deleteContentInfo(content_idx);
+	}
 }
+	
+	
+	
+	
